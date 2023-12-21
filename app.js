a23ebf5e1dab815586b865f40143f63cb1872ca9
Marisa Espinoza <marisaa94@gmail.com>
2023-12-20 19:45:41 -0800

    added error handling

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const csrf = require('csurf');
 
 const routes = require('./routes');
 const { environment } = require("./config");
+const { ValidationError } = require('sequelize');
 
 const isProduction = environment === "production";
 
 const app = express();
 
+//middlewares
 app.use(cors());
 
 app.use(
@@ -19,7 +21,7 @@ app.use(
       policy: "cross-origin" 
     })
   );
-  
+
 app.use(morgan("dev"));
 
 app.use(cookieParser());
@@ -27,7 +29,38 @@ app.use(express.json());
 
 
 app.use(csrf({ cookie: true }))
+//end of middlewares
 
 app.use(routes);
+
+//error handling
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+})
+
+//sequelize error handling
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+})
+
+//error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    })
+})
 
 module.exports = app;
