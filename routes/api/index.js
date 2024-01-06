614670cffdf91850ff8adcec7f7b1220aa7d5b7b
Marisa Espinoza <marisaa94@gmail.com>
2024-01-06 12:01:38 -0800

    added and tested my login route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,42 +1,18 @@
 const router = require('express').Router();
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth.js');
-const { Users } = require('../../db/models');
-const { restoreUser } = require('../../utils/auth.js');
-const { requireAuth } = require('../../utils/auth.js')
+const sessionRouter = require('./session');
+const usersRouter = require('./users');
+const breedsRouter = require('./breeds');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.use('/breeds', breedsRouter);
 
 //test general
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
 
-//test setTokenCookie
-router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
-  const user = await Users.findOne({
-    where: {
-      username: 'dean-tortie'
-    }
-  });
-  setTokenCookie(res, user);
-  return res.json({ user })
-}))
-
-//test restoreUser
-router.get(
-  '/restore-user',
-  restoreUser,
-  (req, res) => {
-    return res.json(req.user);
-  }
-)
-
-//test requireAuth
-router.get(
-  '/require-auth',
-  requireAuth,
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
 
 module.exports = router;
--- routes/api/session.js
+++ routes/api/session.js
@@ -1,4 +1,36 @@
 const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, restorUser } = require('../../utils/auth');
+const { Users } = require('../../db/models');
+
 const router = express.Router();
-//login, logout
+//login, logout routes
+
+//login
+router.post(
+    '/',
+    asyncHandler(async (req, res, next) => {
+        const { credential, password } = req.body;
+        
+        const user = await Users.login({ credential, password });
+
+        if (!user) {
+            const err = new Error('Login failed');
+            err.status = 401;
+            err.title = 'Login failed';
+            err.errors = ['The provided credentials were invalid.'];
+            return next(err);
+        }
+
+        await setTokenCookie(res, user);
+
+        return res.json({
+            user
+        })
+    })
+);
+
+
+
 module.exports = router;
