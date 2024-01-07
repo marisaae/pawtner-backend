cd8ed42761b90a853dea3156d6a3eb82898377d7
Marisa Espinoza <marisaa94@gmail.com>
2024-01-07 10:36:02 -0800

    added and tested signup route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,26 @@
-const express = require('express');
+const express = require("express");
+const asyncHandler = require("express-async-handler");
+
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { Users } = require("../../db/models");
+
 const router = express.Router();
+
 //signup, profile, user's pet preference, user's saved pets
+
+//signup
+router.post(
+    '/',
+    asyncHandler(async (req, res) => {
+        const { firstName, lastName, username, email, password } = req.body;
+        const user = await Users.signup({ firstName, lastName, username, email, password });
+        
+        await setTokenCookie(res, user);
+
+        return res.json({
+            user
+        })
+    })
+);
+
 module.exports = router;
